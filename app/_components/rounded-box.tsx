60a0a496ab78ccf59c0d2b61bfda67a249e1ce1f
keuvyntdev <keuvyntdev@gmail.com>
2025-03-14 10:18:47 -0300

    feat: add rounded-box component responsiveness

--- app/_components/rounded-box.tsx
+++ app/_components/rounded-box.tsx
@@ -13,9 +13,20 @@ export const RoundedBoxContent = ({
 
 const RoundedBox = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="relative z-50 rounded-tl-3xl rounded-tr-3xl bg-white py-5">
-      {children}
-    </div>
+    <>
+      {/* Layout para Mobile */}
+      <div className="block lg:hidden">
+        <div className="relative z-50 rounded-tl-3xl rounded-tr-3xl bg-white py-5">
+          {children}
+        </div>
+      </div>
+      {/* Layout para Desktop */}
+      <div className="hidden lg:block">
+        <div className="relative z-50 mt-[-1.2rem] rounded-tl-3xl rounded-tr-3xl bg-white py-5">
+          {children}
+        </div>
+      </div>
+    </>
   );
 };
 
