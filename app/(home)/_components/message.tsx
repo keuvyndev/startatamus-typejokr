46533db05dcf89a489af32a8504bd997a21bab66
keuvyntdev <keuvyntdev@gmail.com>
2025-03-14 09:51:23 -0300

    feat: add message and input component

---
+++ app/(home)/_components/message.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+interface MessagesProps {
+  message: string;
+  info?: boolean;
+}
+
+const Message = ({ message, info }: MessagesProps) => {
+  return (
+    <div className="flex flex-row items-center gap-2">
+      <Image
+        src="/iconChuck.png"
+        alt="Type Jokr Logo"
+        height={60}
+        width={60}
+        className="object-scale-down"
+      />
+      <span className="text-[15px] font-medium">
+        {info ? message : `“${message}”`}
+      </span>
+    </div>
+  );
+};
+
+export default Message;
---
+++ app/(home)/_components/search-joke-input.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import { Input } from "@/app/_components/ui/input";
+import { JokeContext } from "@/app/_context/joke";
+import { SearchIcon } from "lucide-react";
+import { useContext, useState } from "react";
+
+const SearchJokeInput = () => {
+  const [params, setParams] = useState("");
+  const { getJokeBySearch } = useContext(JokeContext);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setParams(event.target.value);
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(event);
+    console.log(params);
+    await getJokeBySearch(params);
+  };
+
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-full items-center justify-between"
+      >
+        <Input
+          type="joke"
+          placeholder="Search for joke..."
+          className="hover:border-primary w-[90%]"
+          onChange={handleInputChange}
+          value={params}
+        />
+        <Button type="submit" className="bg-primary">
+          <SearchIcon />
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default SearchJokeInput;
